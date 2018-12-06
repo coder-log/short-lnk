4e70b8ca913fc2e962804e43412c8a3d8e6263cf
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-06 08:47:03 +0100

    Adding firsts React components

--- client/main.js
+++ client/main.js
@@ -1,3 +1,15 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Signup from './../imports/ui/Signup';
+import Link from './../imports/ui/Link';
 
+
+Meteor.startup(() => {
+  ReactDOM.render(
+    <div>
+    <Signup/> 
+    <Link/>
+    </div>, document.getElementById('app')
+  );
+});
