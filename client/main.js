565ec27144449f2cb4614b5bdfaec020f2df478d
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-07 06:54:02 +0100

    setting up react router

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Signup from './../imports/ui/Signup';
 import Link from './../imports/ui/Link';
+import { Router, Route, browserHistory} from 'react-router';
 
 
 Meteor.startup(() => {
