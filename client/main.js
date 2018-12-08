b58617b2ac4fa21a636d26f9c51d9c84f5edb0bb
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-08 08:22:04 +0100

    Trying to migrate to React Router v4

--- client/main.js
+++ client/main.js
@@ -3,14 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Signup from './../imports/ui/Signup';
 import Link from './../imports/ui/Link';
-import { Router, Route, browserHistory} from 'react-router';
+//import { Router, Route, browserHistory} from 'react-router';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { browserHistory } from 'react-router-dom';
 
+const routes = (
+  <Router history = {browserHistory}>
+    <Route exact path='/signup' component={Signup} />
+  </Router>
+);
 
 Meteor.startup(() => {
   ReactDOM.render(
-    <div>
-    <Signup/> 
-    <Link/>
-    </div>, document.getElementById('app')
+    routes, document.getElementById('app')
   );
 });
