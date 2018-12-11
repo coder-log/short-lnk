79764ae23f2256eacd20cfd7af551a457fa5a04f
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-11 06:56:36 +0100

    Setting decrement method to decrease the count of the state

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -13,12 +13,20 @@ export default class Signup extends React.Component {
         count: this.state.count + 1
       });
     }
+
+    decrement(){
+      this.setState({
+        count: this.state.count - 1
+
+      });
+    }
     render(){
       return (
       <div>
         <h1>Sign Up to Short Lnk</h1>
         <p>{this.state.count}</p>
         <button onClick={this.increment.bind(this)}>+1</button>
+        <button onClick={this.decrement.bind(this)}>-1</button>
         <Link to="/">Already have an account?</Link>
       </div>
       );
