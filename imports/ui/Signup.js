d69f2814bd1edc537e1c77b8886044e56c3d6f69
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-11 08:21:09 +0100

    Setting the sign up form

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -5,28 +5,26 @@ export default class Signup extends React.Component {
     constructor(props){
       super(props);
       this.state = {
-        count:0
-      };
-    }
-    increment(){
-      this.setState ({
-        count: this.state.count + 1
-      });
+        error: ''      };
     }
 
-    decrement(){
-      this.setState({
-        count: this.state.count - 1
-
+    onSubmit(e) {
+      e.preventDefault();
+      this.setState( {
+        error: 'Something went wrong.'
       });
     }
+    
     render(){
       return (
       <div>
         <h1>Sign Up to Short Lnk</h1>
-        <p>{this.state.count}</p>
-        <button onClick={this.increment.bind(this)}>+1</button>
-        <button onClick={this.decrement.bind(this)}>-1</button>
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+        <form onSubmit={this.onSubmit.bind(this)} >
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <button>Create Account</button>
+        </form>
         <Link to="/">Already have an account?</Link>
       </div>
       );
