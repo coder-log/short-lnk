84c0adec7d7cdd11b6104d408aa90ccf34951d9b
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-13 06:51:38 +0100

    Settin login

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import {Link} from'react-router-dom';
+import { Meteor } from 'meteor/meteor';
+
 
 export default class Login extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+          error: ''      
+        };
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+  
+        let email = this.refs.email.value.trim();
+        let password = this.refs.password.value.trim();
+  
+        Meteor.loginWithPassword({email}, password, (err)=> {
+            console.log("Login Callback", err);
+        });
+  
+        // this.setState( {
+        //   error: 'Something went wrong.'
+        // });
+      }
+
+
+
     render() {
         return (
         <div>
             <h1> Login to Short Lnk</h1>
+            {this.state.error ? <p>{this.state.error}</p> : undefined}
+        <form onSubmit={this.onSubmit.bind(this)} >
+          <input type="email" ref="email" name="email" placeholder="Email" />
+          <input type="password" ref="password" name="password" placeholder="Password" />
+          <button>Login</button>
+        </form>
              
             <Link to="/signup">Have an account?</Link>
         </div>
