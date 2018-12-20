025d1dd9e90dd5e34c69bba7e413c788dff28cd6
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-20 06:37:40 +0100

    Uninstalling simpl-schema, app crashing

--- server/main.js
+++ server/main.js
@@ -1,28 +1,31 @@
 import { Meteor } from 'meteor/meteor';
-// import SimpleSchema from 'simpl-schema';
+import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
-  // //code to run on server at startup
-  // const employeeSchema = new SimpleSchema ({
-  //   name:{
-  //     type:String,
-  //     min:1,
-  //     max:200
-  //   },
-  //   hourlyWage:{
-  //     type:Number,
-  //     min:0
-  //   },
-  //   email:{
-  //     type:String,
-  //     regEx:SimpleSchema.RegEx.Email
-  //   }
+  //code to run on server at startup
+  const employeeSchema = new SimpleSchema ({
+    name:{
+      type:String,
+      min:1,
+      max:200
+    },
+    hourlyWage:{
+      type:Number,
+      min:0
+    },
+    email:{
+      type:String,
+      regEx:SimpleSchema.RegEx.Email
+    }
 
-  // });
+  });
 
 
-  // employeeSchema.validate({
+  employeeSchema.validate({
+    name: 'Mike',
+    hourlyWage: 35,
+    email: 'mikepurple'
     
-  // });
+  });
   
 });
