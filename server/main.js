0e2b74dc95ab4af25e1b99083e547993f0152ea5
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-22 07:18:06 +0100

    adding email validation for users signup/login

--- server/main.js
+++ server/main.js
@@ -1,31 +1,45 @@
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
+import { Accounts }  from 'meteor/accounts-base';
 
 
 Meteor.startup(() => {
   //code to run on server at startup
-  const employeeSchema = new SimpleSchema ({
-    name:{
-      type: String,
-      min: 1,
-      max: 200
-    },
-    hourlyWage:{
-      type: Number,
-      min: 0
-    },
-    email:{
-      type:String,
-      regEx: SimpleSchema.RegEx.Email //this line breaks the app
-    }
 
+  Accounts.validateNewUser((user)=>{
+    const email = user.emails[0].address;
+
+    new SimpleSchema({
+      email: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Email
+      }
+    }).validate({email})
+    return true;
   });
 
+  // const employeeSchema = new SimpleSchema ({
+  //   name:{
+  //     type: String,
+  //     min: 1,
+  //     max: 200
+  //   },
+  //   hourlyWage:{
+  //     type: Number,
+  //     min: 0
+  //   },
+  //   email:{
+  //     type:String,
+  //     regEx: SimpleSchema.RegEx.Email //this line breaks the app
+  //   }
 
-  employeeSchema.validate({
-    name: 'Mike',
-    hourlyWage: 65, //if i try to validate a string instead of a number, it crash the app.
-    email: 'mikepurple@gmail.com'   
-  });
+  // });
+
+
+  // employeeSchema.validate({
+  //   name: 'Mike',
+  //   hourlyWage: 65, //if i try to validate a string instead of a number, it crash the app.
+  //   email: 'mikepurple@gmail.com'   
+  // });
   
 });
