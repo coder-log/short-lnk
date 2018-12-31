9245e1e5a6c01bc1d7a94bb9bcd57cdad1800fab
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-31 07:06:21 +0100

    Adding 'addNumbers' exaple method

--- client/main.js
+++ client/main.js
@@ -13,6 +13,10 @@ Meteor.startup(() => {
   Meteor.call('greetUser', 'Mike', (err,res)=>{
     console.log('Greet user args', err, res);
   });
+  Meteor.call('addNumbers', 3, 4, (err, res)=>{
+    console.log('addNumbers arguments', err, res);
+    
+  });
   ReactDOM.render(
     routes, document.getElementById('app')
   );
--- imports/api/links.js
+++ imports/api/links.js
@@ -13,5 +13,15 @@ Meteor.methods({
     greetUser(name = 'User'){
         console.log('greetUser is running');
         return `Hello ${name}!`;
+    },
+    addNumbers(number1, number2){
+        console.log('addNumbers is running');
+        
+        if(typeof number1 !== 'number' ||typeof number2 !== 'number' ){
+            throw new Meteor.Error('invalid-arguments', 'The arguments should be numbers')
+        }
+        
+        return number1 + number2;
     }
+
 });
