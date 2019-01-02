cbd9170ba2237f0af9409c9c45f9054fa875d876
Xavier <43478582+coder-log@users.noreply.github.com>
2019-01-02 07:05:25 +0100

    End of lesson 73

--- client/main.js
+++ client/main.js
@@ -10,13 +10,7 @@ Tracker.autorun(()=>{
 });
 
 Meteor.startup(() => {
-  Meteor.call('greetUser', 'Mike', (err,res)=>{
-    console.log('Greet user args', err, res);
-  });
-  Meteor.call('addNumbers', 3, 4, (err, res)=>{
-    console.log('addNumbers arguments', err, res);
     
-  });
   ReactDOM.render(
     routes, document.getElementById('app')
   );
