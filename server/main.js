1170038cb305ad4a4687c0033ad05c732623f62a
Xavier <43478582+coder-log@users.noreply.github.com>
2019-01-07 08:44:23 +0100

    Setting 302 redirections dinamically, intercepting the url requested, in middleware searching to the db.

--- server/main.js
+++ server/main.js
@@ -1,9 +1,28 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
+
 import './../imports/api/users';
-import './../imports/api/links';
+import {Links} from '../imports/api/links';
 import '../imports/startup/simple-schema-configuration.js';
 
 Meteor.startup(() => {
   //code to run on server at startup
+  WebApp.connectHandlers.use((req, res, next)=>{ 
+
+    const _id = req.url.slice(1); // assigns variable _id the url requeste minus '/'
+    const link = Links.findOne({_id}); // busca en la base de datos esa url equivale ES6 a {_id: _id}
     
-   });
+    if (link) {
+      res.statusCode = 302;
+      res.setHeader('Location', link.url);
+      res.end();
+    } else{
+      next();
+    }
+    
+    
+
+  });
+
+    
+});
