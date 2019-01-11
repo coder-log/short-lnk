e7b09c2bfc452b78cad21910f5d7b1341d471674
Xavier <43478582+coder-log@users.noreply.github.com>
2019-01-11 15:13:48 +0100

    Adding feature. Copy button of the link to clipboard

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Clipboard from 'clipboard';
+
 
 export default class LinksListItem extends Component {
+  componentDidMount() {
+    const clipboard= new Clipboard(this.refs.copy);
+
+    clipboard.on('success', () =>{
+      alert ('It worked!');
+    }).on('error', ()=> {
+      alert('Unable to copy. Please manually copy the link.');
+    })
+  }
   render() {
     return (
       <div>
           <p>{this.props.url}</p>
           <p>{this.props.shortUrl}</p>
+          <button ref="copy" data-clipboard-text={this.props.short}>Copy</button>
         
       </div>
     );
   }
+};
+
+LinksListItem.propTypes = {
+    _id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    userId: PropTypes.string.isRequired,
+    shortUrl: PropTypes.string.isRequired,
 };
