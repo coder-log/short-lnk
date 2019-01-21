4f2332b20af7848aa767483ceec12399436e3585
Xavier <43478582+coder-log@users.noreply.github.com>
2019-01-21 05:59:12 +0100

    New component to filter items with a checkbox

---
+++ imports/ui/LinksListFilter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <label>
+                <input type = "checkbox"/>
+                Show hidden links
+            </label>
+        </div>
+
+    );
+};
+
