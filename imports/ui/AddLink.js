2a85f41195327d1fa76beaf02922ff47f357c807
Xavier <43478582+coder-log@users.noreply.github.com>
2019-01-22 06:34:41 +0100

    Converting the AddLink component input from uncontrolled input, to controlled input. Blank spaces not allowed.

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 
 export default class AddLink extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            url: ''
+        };
+    }
+
     onSubmit(e){
         const url = this.refs.url.value.trim();
         e.preventDefault();
@@ -13,12 +21,22 @@ export default class AddLink extends Component {
         }
 
     }
+    onChange(e) {
+        this.setState({
+            url:e.target.value.trim()
+        });
+    }
     render() {
         return (
         <div>
             <p>Add Link</p>
             <form onSubmit = {this.onSubmit.bind(this)}>
-                <input type="text" ref="url" placeholder="URL"/>
+                <input 
+                    type="text" 
+                    ref="url" 
+                    placeholder="URL" 
+                    value={this.state.url}
+                    onChange={this.onChange.bind(this)}/>
                 <button>Add Link</button>
             </form>
         </div>
